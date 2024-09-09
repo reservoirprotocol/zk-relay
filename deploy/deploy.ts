2cede61df0da11dac02266cead654f222f1d34a2
stephankmin <stephankmin@gmail.com>
2024-09-09 17:58:48 -0400

    scripts: add seaport 1.6 deploy script

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -2,9 +2,10 @@ import { deployContract } from "./utils";
 import { DeploymentType } from "zksync-ethers/build/types";
 
 export default async function () {
-  
+  await deploySeaportV1_6();
 }
-const salt = "0x0000000000000000000000000000000000000000000000000000000000000001";
+
+const salt = "0x0000000000000000000000000000000000000000000000000000000000000004";
 
 const deployWETH9 = async () => {
   await deployContract(
@@ -46,8 +47,8 @@ const deployConduitController = async () => {
 }
 
 const deploySeaportV1_6 = async () => {
-  await deployContract(
-    "Seaport",
+  const conduitController = await deployContract(
+    "contracts/seaport-1.6/conduit/ConduitController.sol:ConduitController",
     "create2" as DeploymentType,
     [], // constructorArguments (empty array if there are no constructor arguments)
     {}, // options (empty object if no options are needed)
@@ -56,4 +57,21 @@ const deploySeaportV1_6 = async () => {
       salt: salt
     }}
   );
+
+  const conduitControllerAddress = await conduitController.getAddress();
+  console.log(
+    `ConduitController deployed to address ${conduitControllerAddress}`
+  );
+
+  const seaport = await deployContract(
+    "contracts/seaport-1.6/Seaport.sol:Seaport",
+    "create2" as DeploymentType,
+    [conduitControllerAddress], // constructorArguments (empty array if there are no constructor arguments)
+    {}, // options (empty object if no options are needed)
+    {
+      customData: {
+      salt: salt
+    }}
+  );
+  console.log(`Seaport deployed to address ${seaport.getAddress()}`);
 }
