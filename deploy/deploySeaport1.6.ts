c316a811e7efc4a5ff7b36172977d2f11beec427
stephankmin <stephankmin@gmail.com>
2024-10-21 20:29:49 -0400

    scripts: add new conduit controller addr

--- deploy/deploySeaport1.6.ts
+++ deploy/deploySeaport1.6.ts
@@ -2,7 +2,7 @@ import { deployContract } from "./utils";
 import { DeploymentType } from "zksync-ethers/build/types";
 
 const salt = "0x0000000000000000000000000000000000000000000000000000000000000000";
-const CONDUIT_CONTROLLER_ADDRESS = "0xA453E092951863fb829781643Ea692188ee5C7E1"
+const CONDUIT_CONTROLLER_ADDRESS = "0xee8810654aDd44297Cb0508288F6C2050890FC81"
 
 export default async function () {
   await deploySeaportV1_6();
